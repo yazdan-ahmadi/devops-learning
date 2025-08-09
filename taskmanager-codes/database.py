8414ee9eeaa0980fa65406a4cdd73d2bd9e5378c
mohammad yazdan ahmadi <yazdan7988@gmail.com>
2025-08-09 16:29:40 +0330

    Update database.py

--- taskmanager-codes/database.py
+++ taskmanager-codes/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = "postgresql://python:python123@my-postgres-postgresql:5432/python"
+DATABASE_URL = "postgresql://<<postgresql_username>>:<<postgresql_password>>@<<postgresql_service_name>>:5432/<<database_name>>"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 
